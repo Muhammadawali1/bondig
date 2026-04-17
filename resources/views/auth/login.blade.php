e6e7bc83f669cada2b8c8ce6543f2b877bf81dbe
Muhammad Awali <muhammadraihan9222@gmail.com>
2026-04-17 11:49:15 +0700

    Fix refreshCaptcha to use 'flat' type instead of hardcoded 'default'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -213,9 +213,8 @@ function togglePassword(fieldId, button) {
 
 function refreshCaptcha(id = 'captcha-img') {
     const captcha = document.getElementById(id);
-    captcha.src = "{{ captcha_src() }}" + '?' + Date.now();
+    captcha.src = "{{ captcha_src('flat') }}" + '?' + Date.now();
 }
 </script>
 
 </html>
-
